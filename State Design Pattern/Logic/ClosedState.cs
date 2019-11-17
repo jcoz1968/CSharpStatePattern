2dc9a4f36d100b5a0e0468697059095992ceeef4
Jeffery Cosby <jcoz1968@gmail.com>
2019-11-17 13:24:06 -0600

    Context and State

---
+++ State Design Pattern/Logic/ClosedState.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State_Design_Pattern.Logic
+{
+    class ClosedState : BookingState
+    {
+        public override void Cancel(BookingContext booking)
+        {
+        }
+
+        public override void DatePassed(BookingContext booking)
+        {
+        }
+
+        public override void EnterDetails(BookingContext booking, string attendee, int ticketCount)
+        {
+        }
+
+        public override void EnterState(BookingContext booking)
+        {
+
+        }
+    }
+}
